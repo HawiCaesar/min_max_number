93140679f9379ce26ffe2284a30c562340343b1d
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-14 12:46:54 +0300

    added logic to min_max_number, tested with testcase

--- min_max_number.py
+++ min_max_number.py
@@ -1,23 +1,28 @@
+"""
+Returns a list of the min and max of a list of numbers.
+Takes one argument, a list of numbers
 
+sorts the numbers from ascending 
+to descedning order then get get min and max number
+
+"""
 def find_max_min(list_of_numbers):
 
 	list_of_min_max_numbers = []
 
-	max_number = max(list_of_numbers)
-	min_number = min(list_of_numbers)
+	list_of_numbers.sort() # Sort from lowest to highest
 
-	if max_number == min_number:
-		#list_of_min_max_numbers[max_number]
-		list_length = len(list_of_numbers)
-		list_of_min_max_numbers.append(list_length)
 
+	# check if the min number is the same as the max number and return the length of the list
+	if list_of_numbers[0] == list_of_numbers[-1]:
+
+		list_of_min_max_numbers.append(len(list_of_numbers))
+
+	# Add to new list
 	else:
-		list_of_min_max_numbers.append(min_number)
-		list_of_min_max_numbers.append(max_number)
+		list_of_min_max_numbers.append(list_of_numbers[0])
+		list_of_min_max_numbers.append(list_of_numbers[-1])
 
 		
 	return list_of_min_max_numbers
-
-
-
 
